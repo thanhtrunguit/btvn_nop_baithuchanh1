5673a2a6baecafc8f57f91d0387bd8e29235dcf4
Thanh Trung Ngo <thanhtrung@192.168.2.6>
2022-10-16 21:09:29 +0700

    first commit

---
+++ a.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout << "put in your birth year: " << endl;
+    cin >> year;
+    cout << "your age is: " << 2022 - year;
+}
